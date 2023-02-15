43d32e73a10f0d69a8e80ef9a45e91c0a2480da5
Bitaru <i@bitaru.com>
2023-02-15 13:39:37 +0100

    separate build and book

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,13 +13,6 @@ export const parameters = {
   backgrounds: {
     default: 'light',
   },
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
 }
 
 export const render = createLitRenderer({
