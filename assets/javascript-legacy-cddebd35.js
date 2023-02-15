9ed752a13606b47138a3a1f3be66405803ffcc83
Bitaru <i@bitaru.com>
2023-02-15 13:49:54 +0100

    Updates

---
+++ assets/javascript-legacy-cddebd35.js
@@ -0,0 +1,9 @@
+System.register(["./typescript-legacy-9dbc75e3.js","./editor.api-legacy-70b8f0db.js","./format-legacy-da166fcf.js"],(function(e,t){"use strict";var s,i;return{setters:[e=>{s=e.conf,i=e.language},null,null],execute:function(){
+/*!-----------------------------------------------------------------------------
+       * Copyright (c) Microsoft Corporation. All rights reserved.
+       * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
+       * Released under the MIT license
+       * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+       *-----------------------------------------------------------------------------*/
+e("conf",s),e("language",{defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:i.operators,symbols:i.symbols,escapes:i.escapes,digits:i.digits,octaldigits:i.octaldigits,binarydigits:i.binarydigits,hexdigits:i.hexdigits,regexpctl:i.regexpctl,regexpesc:i.regexpesc,tokenizer:i.tokenizer})}}}));
+//# sourceMappingURL=javascript-legacy-cddebd35.js.map
---
+++ sb-addons/storybook-8/manager-bundle.mjs
@@ -0,0 +1,4 @@
+try{
+var s=__STORYBOOKAPI__,{ActiveTabs:a,Consumer:n,ManagerContext:u,Provider:l,addons:r,combineParameters:S,controlOrMetaKey:c,controlOrMetaSymbol:i,eventMatchesShortcut:m,eventToShortcut:h,isMacLike:y,isShortcutTaken:d,keyToSymbol:p,merge:b,mockChannel:O,optionOrAltSymbol:T,shortcutMatchesShortcut:A,shortcutToHumanString:_,types:g,useAddonState:k,useArgTypes:M,useArgs:P,useChannel:f,useGlobalTypes:v,useGlobals:C,useParameter:x,useSharedState:K,useStoryPrepared:B,useStorybookApi:G,useStorybookState:I}=__STORYBOOKAPI__;r.setConfig({isFullscreen:!1,showNav:!1});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.mjs.map
